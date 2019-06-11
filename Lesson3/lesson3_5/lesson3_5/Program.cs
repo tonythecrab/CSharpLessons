bd7df212c03ed7994e4e942852d6e700b5a181f4
Last_Christmas <misserschmitt@gmail.com>
2019-06-11 12:43:47 +0300

    Final version 3-5

--- Lesson3/lesson3_5/lesson3_5/Program.cs
+++ Lesson3/lesson3_5/lesson3_5/Program.cs
@@ -10,22 +10,49 @@ namespace lesson3_5
     {
         static void Main(string[] args)
         {
-           // Generate a random number between two numbers
+            // Generate a random number between two numbers
             int RandomNumber(int min, int max)
             {
                 Random random = new Random();
                 return random.Next(min, max);
             }
-            
-            Console.WriteLine("Select min border");
-            int minValue = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Select max border");
-            int maxValue = Convert.ToInt32(Console.ReadLine());
 
+            try
+            {
+                Console.WriteLine("Select min border");
+                int minValue = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Select max border");
+                int maxValue = Convert.ToInt32(Console.ReadLine());
+            
+            
             do
-            { RandomNumber(minValue, maxValue); }
+            {
+                int random = RandomNumber(minValue, maxValue);
+                int attempt = 3;
+                Console.WriteLine("New round.");
+                do
+                {
+                    Console.WriteLine("Enter a number");
+                    int value = Convert.ToInt32(Console.ReadLine());
+                    if (value == random)
+                    {
+                        Console.WriteLine("Correct.");
+                        break;
+                    }
+                    else
+                    {
+                        attempt--;
+                        Console.WriteLine($"Wrong number. Attempts left: {attempt}");
+                    }
+                } while (attempt > 0);
+                
+            } while (true == true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} wrong input", e);
+            }
 
-            
-        }
+            }
     }
 }
