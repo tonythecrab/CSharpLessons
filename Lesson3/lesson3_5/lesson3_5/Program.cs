23df8534fc2827b7539623d7dd162c48bbb97d0f
Last_Christmas <misserschmitt@gmail.com>
2019-06-11 11:51:23 +0300

    3_5

---
+++ Lesson3/lesson3_5/lesson3_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Generate a random number between two numbers
+            int RandomNumber(int min, int max)
+            {
+                Random random = new Random();
+                return random.Next(min, max);
+            }
+            
+            Console.WriteLine("Select min border");
+            int minValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Select max border");
+            int maxValue = Convert.ToInt32(Console.ReadLine());
+
+            do
+            { RandomNumber(minValue, maxValue); }
+
+            
+        }
+    }
+}
