862e6870b418a58789be837d372d068b7034a655
Last_Christmas <misserschmitt@gmail.com>
2019-06-13 12:47:53 +0300

    Car2

--- Lesson4/CarConstruct/CarConstruct/Program.cs
+++ Lesson4/CarConstruct/CarConstruct/Program.cs
@@ -23,71 +23,80 @@ namespace CarConstruction
             var engine2 = new Engine(engineValue2);
             var transmission1 = new Transmission(transmissionValue);
             var color1 = new Color(colorValue);
-            var car1 = new Car(engine1, color1, transmission1);
-            car1.getCarInfo();
-            var upgrade = new CarConstruct(car1);
+
+            var car = CarConstruct.Construct(engine1, color1, transmission1);
+            
+            car.getCarInfo();
+
+            CarConstruct.Reconstruct(car, engine2);
+                car.getCarInfo();
+            //var upgrade = new CarConstruct(car1);
             Console.ReadKey();
         }
 
     }
 
 }
-    
-    class Engine
-    {
-        public string engineType { get; set; }
 
-        public Engine(string EngineType)
-        {
-            engineType = EngineType;
-        }
+class Engine
+{
+    public string engineType { get; set; }
+
+    public Engine(string EngineType)
+    {
+        engineType = EngineType;
+    }
+}
+
+class Color
+{
+    public string colorValue { get; set; }
+
+    public Color(string ColorValue)
+    {
+        colorValue = ColorValue;
+    }
+}
+
+class Transmission
+{
+    public string TransmissionType { get; set; }
+
+    public Transmission(string transmissionType)
+    {
+        transmissionType = TransmissionType;
+    }
+}
+
+class CarConstruct
+{
+
+    public static Car Construct(Engine engine, Color color, Transmission transmission)
+    {
+
+        var car1 = new Car(engine, color, transmission);
+        return car1;
     }
 
-    class Color
+    public static void Reconstruct(Car car, Engine engine)
     {
-        public string colorValue { get; set; }
-
-        public Color(string ColorValue)
-        {
-            colorValue = ColorValue;
-        }
+        car.engine = engine;
     }
+}
 
-    class Transmission
+class Car
+{
+    public Engine engine {  get; set; }
+    Color color;
+    Transmission transmission;
+    public Car(Engine eng, Color col, Transmission trans)
     {
-        public string TransmissionType { get; set; }
-
-        public Transmission(string transmissionType)
-        {
-            transmissionType = TransmissionType;
-        }
+        engine = eng; color = col; transmission = trans;
     }
-
-    class CarConstruct
+    public void getCarInfo()
     {
-
-
-        Engine engine;
-       
-        public CarConstruct(Car value, Engine eng)
-        {
-        
-        }
+        Console.WriteLine($"Car with {engine}, {transmission} type and {color} color");
     }
-
-    class Car
-    {
-        Engine engine;
-        Color color;
-        Transmission transmission;
-        public Car(Engine eng, Color col, Transmission trans )
-        {
-            engine = eng; color = col; transmission = trans;
-        }
-        public void getCarInfo()
-        {
-            Console.WriteLine($"Car with {engine}, {transmission} type and {color} color");
-        }
-    }
+}
 
 
