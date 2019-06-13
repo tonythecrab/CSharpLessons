2f62fe4d967bab8b2bf008abbeece0f8999e4039
Last_Christmas <misserschmitt@gmail.com>
2019-06-13 13:09:06 +0300

    Car classes

---
+++ Lesson4/CarConstruct/CarConstruct/Engine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarConstruction
+{
+    public class Engine
+    {
+        public string engineType { get; set; }
+
+        public Engine(string EngineType)
+        {
+            engineType = EngineType;
+        }
+    }
+
+}
