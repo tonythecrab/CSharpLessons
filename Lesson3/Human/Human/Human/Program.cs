c3c9ea793ca817f7ce333973f19e1127106f6f5c
Last_Christmas <misserschmitt@gmail.com>
2019-06-13 10:09:45 +0300

    Human

---
+++ Lesson3/Human/Human/Human/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human1 = new Human("Bob");
+            Human human2 = new Human("Bob");
+            Human.compare(human1, human2);
+            Console.ReadLine();
+        }
+
+    }
+
+    class Human
+    {
+        public int BirthDate;
+        public string FirstName;
+        public string LastName;
+        public int Age { get; }
+
+        public Human() { FirstName = "undefined"; }
+        public Human(string fname) { FirstName = fname; BirthDate = 1900; }
+
+        public Human(string fname, string lname, int age) { FirstName = fname; LastName = lname; }
+
+        public void getInfo()
+        {
+            Console.WriteLine($"Human {LastName + FirstName} burn in {BirthDate}, now {Age} years old");
+        }
+
+        public static void compare(Human value1, Human value2)
+        {
+            if (value1 == value2)
+            {
+            Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");//всегда False? 
+            };
+        }
+             
+        
+
+    }
+}
