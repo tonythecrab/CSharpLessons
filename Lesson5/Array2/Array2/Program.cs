bf9b1edf47e7c8c1f87b616a292207ffc61ca1df
Last_Christmas <misserschmtt@gmail.com>
2019-06-24 10:47:54 +0300

    Half of: Array task Lesson 5. Interface task Lesson 6

---
+++ Lesson5/Array2/Array2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace Array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arr NewArray = new Arr(1, 2, 3, 4);
+            PrintArray(NewArray);  // почему возвращает Array2.Arr ?
+            var answer = NewArray.Contains(5);
+            Console.WriteLine($"{answer}");
+            Console.ReadLine();
+        }
+        
+
+
+
+        public static void PrintArray(params Arr[] Arr)
+        {
+            foreach (var i in Arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    public class Arr
+    {
+        public int[] Array { get; set; }
+        public Arr(params int[] ArrayIn)
+        {
+            Array = ArrayIn;
+        }
+        public bool Contains(int value)
+        {
+            foreach (int i in Array)
+            {
+                if (i == value) { return true; }
+            }
+            return false;
+        }
+        
+
+    }
+}
---
+++ Lesson6/Interface/Interface/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string pass;
+            string email;
+            do
+            {
+                name = Console.ReadLine();
+                pass = Console.ReadLine();
+                email = Console.ReadLine();
+                if (name != "exit" && pass != "exit")
+                {
+                    var user = new User(name, pass, email);
+
+                    Users[] users = new Users[10];
+                    for (int i = 0; i < i+1; i++)
+                    {
+                        users[i] = user;
+                    }
+                       
+                    Console.WriteLine($"Welcome to the Matrix {user.Name}");
+                }
+
+
+            } while (name != "exit" && pass != "exit");
+        }
+
+        public class Users { }
+
+        public interface IUser
+        {
+            string Name { get; set; }
+            string Password { get; set; }
+            string Email { get; set; }
+
+            //string GetFullInfo();
+            
+        }
+        interface IValidator
+        {
+            void ValidateUser();
+        }
+
+        public class User : Users, IUser
+        {
+            public User(string name, string password, string email)
+            {
+                Name = name;
+                Password = password;
+                Email = email;
+            }
+            public string Name { get; set; }
+            public string Password { get; set; }
+            public string Email { get; set; }
+
+        }
+    }
+}
