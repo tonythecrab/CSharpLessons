362d589bfd7acb46e75e628c35da99c568f9cef8
Last_Christmas <misserschmtt@gmail.com>
2019-07-08 10:59:45 +0300

    LinkedList implementation

---
+++ Lesson7/LinkedList/LinkedList/DoublyNode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class DoublyNode<T>
+    {
+        public LinkedList<T> NewList = new LinkedList<T>();
+
+        public void Add(T data)
+        {
+            NewList.AddLast(data);
+        }
+        public bool Remove(T data)
+        {
+            return NewList.Remove(data);
+        }
+        public int counter(LinkedList<T> list)
+        {
+            int _counter = 0;
+            foreach (var i in list)
+
+            {
+                _counter++;
+            }
+            return _counter;
+
+        }
+        public void ToArray(LinkedList<T> list)
+        {
+            var arrayLength = counter(list);
+            T[] array = new T[arrayLength];
+
+            int j = 0;
+            foreach (var i in list)
+            {
+
+                array[j] = i;
+                j++;
+
+            }
+
+        }
+
+    }
+}
---
+++ Lesson7/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choise = null;
+
+            var listClass = new DoublyNode<string>();
+            while (true)
+            {
+                Console.WriteLine("Pleasse chose one option " +
+                    "\n press 1 to Add item " +
+                    "\n press 2 to item " +
+                    "\n press 3 to count all existing items " +
+                    "\n press 4 to create an array of existing items");
+                choise = Console.ReadLine();
+
+                switch (choise)
+                {
+                    case "1":
+                        Console.WriteLine("Please enter a value");
+                        var item = Console.ReadLine();
+                        listClass.Add(item);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Please enter a value");
+                        item = Console.ReadLine();
+                        listClass.Remove(item);
+                        break;
+                    case "3":
+
+                        var counter = listClass.counter(listClass.NewList);
+                        Console.WriteLine(counter);
+                        break;
+
+                    case "4":
+
+                        listClass.ToArray(listClass.NewList);
+                        break;
+                }
+            }
+        }
+    }
+}
